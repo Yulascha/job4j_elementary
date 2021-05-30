a662b169f5769815a952902418e416e939261c49
Yulascha <yulascha@mail.ru>
2021-05-30 20:03:44 +0300

    4.1. Аргументы [#295521]

--- src/ru/job4j/ArgMethod.java
+++ src/ru/job4j/ArgMethod.java
@@ -4,6 +4,7 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
+
     public static void hello(int age) {
         System.out.println(age);
     }
