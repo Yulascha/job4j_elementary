08abb3183067bd9a6827645c410930e1bea2fce9
Yulascha <yulascha@mail.ru>
2021-06-02 12:49:12 +0300

    4.1.2. Глупый бот [#31323 #163329]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -6,11 +6,8 @@ public class DummyBot {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        }
-        if ("Пока.".equals(question)) {
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
-        } else if ("Сколько будет 2 + 2?".equals(question)) {
-        rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
     }
