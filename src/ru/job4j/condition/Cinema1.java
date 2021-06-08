991bba3ca93ff7e4d2efcbf0b07e621ea2153f83
Yulascha <yulascha@mail.ru>
2021-06-08 14:03:06 +0300

    4.3.1. Оператор && [#333783]

---
+++ src/ru/job4j/condition/Cinema1.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Cinema1 {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent = true);
+        if (hasMoney = true);
+            boolean permission = allowByParent && hasMoney; {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
