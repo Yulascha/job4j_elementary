c36a326648810ee9d04c65eb0e1046e742a1df0a
Yulascha <yulascha@mail.ru>
2021-06-22 12:40:42 +0300

    5.4. Протеиновая диета [#156313]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -4,7 +4,11 @@ public class Fitness {
 
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while ((ivan *= 3) < (nik *= 2));
+        while (ivan <= nik) {
+            ivan = (ivan *= 3);
+            nik = (nik *= 2);
+            month++;
+        }
         return month;
     }
 
