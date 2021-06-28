ce69a77d27982b2a9e04e7c0006f79ce52b9caa0
Yulascha <yulascha@mail.ru>
2021-06-28 16:30:29 +0300

    6.1. Классический поиск перебором. [#33489]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
-        for (int index = 0; index < el.length; index++) {
+        for (int index = 0; index < data; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
@@ -13,7 +13,7 @@ public class FindLoop {
     }
 
     public static void main(String[] args) {
-        int[] array = indexOf({5, 4, 3, 2}, 5);
-        System.out.println(rst);
+        int result = FindLoop.indexOf({5, 4, 3, 2}, 5);
+        System.out.println();
     }
 }
