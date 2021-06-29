ba1672ce3aae989c617768232387970500a98a31
Yulascha <yulascha@mail.ru>
2021-06-29 22:54:38 +0300

    6.4. Слово начинается с ... [#41585 #172820]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,10 +4,9 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        if (char.startsWith(word, pref)) {
-            return true;
-        } else {
-            return false;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]);
+            return result;
         }
         return result;
     }
