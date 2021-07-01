2154efe1e1e00950905f72adc8829d09dae69b90
Yulascha <yulascha@mail.ru>
2021-07-01 20:23:37 +0300

    1

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
         for (int number = count; number >= finish; number++) {
-            if (CheckPrimeNumber.check(number)) {
+            if (CheckPrimeNumber.check(number)){
             }
         }
         return count;
